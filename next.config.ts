f59d782c54d45408dd364f8421f2cc3c2854193d
Santeri <santeri.ora@tuni.fi>
2025-05-27 01:16:16 +0300

    Moved redirect to NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: '/tiktok/callback',
+        destination: 'http://localhost:3000/tiktok/callback',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
